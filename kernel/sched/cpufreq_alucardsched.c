804656f0f4c1e06c9392420a877d514aefbf6c13
Mirenk <minikmwinsea@gmail.com>
2019-08-28 09:08:40 +0900

    cpufreq_alucardsched: Don't use capacity

--- kernel/sched/cpufreq_alucardsched.c
+++ kernel/sched/cpufreq_alucardsched.c
@@ -32,7 +32,7 @@
 #define cpufreq_disable_fast_switch(x)
 #define ACGOV_KTHREAD_PRIORITY	50
 
-#ifdef CONFIG_ARCH_SDM845
+#ifdef CONFIG_ARCH_DUMMY
 #define UP_RATE_LIMIT_US			(20000)
 #define UP_RATE_LIMIT_US_BIGC		(10000)
 #define DOWN_RATE_LIMIT_US			(20000)
@@ -44,7 +44,7 @@
 #define BOOST_PERC					10
 #else
 #define LATENCY_MULTIPLIER			(2000)
-#define FREQ_RESPONSIVENESS			1036800
+#define FREQ_RESPONSIVENESS			1056000
 #define PUMP_INC_STEP_AT_MIN_FREQ	1
 #define PUMP_INC_STEP				1
 #define PUMP_DEC_STEP_AT_MIN_FREQ	1
@@ -117,7 +117,7 @@ struct acgov_cpu {
 static DEFINE_PER_CPU(struct acgov_cpu, acgov_cpu);
 static DEFINE_PER_CPU(struct acgov_tunables, cached_tunables);
 
-#ifdef CONFIG_ARCH_SDM845
+#ifdef CONFIG_ARCH_DUMMY
 #define LITTLE_NFREQS				21
 #define BIG_NFREQS					26
 static unsigned long little_capacity[LITTLE_NFREQS][2] = {
@@ -304,7 +304,7 @@ static unsigned int resolve_target_freq(struct cpufreq_policy *policy,
 	return target_freq;
 }
 
-#ifdef CONFIG_ARCH_SDM845
+#ifdef CONFIG_ARCH_DUMMY
 static void get_target_capacity(unsigned int cpu, int index,
 					unsigned long *down_cap, unsigned long *up_cap)
 {
@@ -357,7 +357,7 @@ static unsigned int get_next_freq(struct acgov_cpu *sg_cpu, unsigned long util,
 	int pump_inc_step = tunables->pump_inc_step;
 	int pump_dec_step = tunables->pump_dec_step;
 	unsigned int next_freq = 0;
-#ifdef CONFIG_ARCH_SDM845
+#ifdef CONFIG_ARCH_DUMMY
 	unsigned long down_cap = 0, up_cap = 0;
 	unsigned long cur_util =
 			util + ((util * tunables->boost_perc) / 100);
@@ -379,7 +379,7 @@ static unsigned int get_next_freq(struct acgov_cpu *sg_cpu, unsigned long util,
 		pump_inc_step = tunables->pump_inc_step_at_min_freq;
 		pump_dec_step = tunables->pump_dec_step_at_min_freq;
 	}
-#ifdef CONFIG_ARCH_SDM845
+#ifdef CONFIG_ARCH_DUMMY
 	get_target_capacity(policy->cpu, index, &down_cap, &up_cap);
 	if (cur_util >= up_cap
 		&& policy->cur < policy->max) {
@@ -1020,7 +1020,7 @@ static void get_tunables_data(struct acgov_tunables *tunables,
 	}
 
 initialize:
-#ifdef CONFIG_ARCH_SDM845
+#ifdef CONFIG_ARCH_DUMMY
 	if (cpu < 2)
 		tunables->up_rate_limit_us = UP_RATE_LIMIT_US;
 	else
